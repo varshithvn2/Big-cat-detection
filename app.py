038079aed5d6c9ef76e5b8a0d5d19cea4d0ada65
Varshith <75238274+varshithvn2@users.noreply.github.com>
2024-01-12 19:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 from roboflow import Roboflow
+import io
 
 # Initialize Roboflow
 rf = Roboflow(api_key="VWJEsV25FRq11fja5SML")
@@ -17,8 +18,11 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
+    # Convert the UploadedFile to bytes
+    image_bytes = uploaded_file.read()
+
     # Make prediction using Roboflow
-    prediction_image = model.predict(uploaded_file).image
+    prediction_image = model.predict(io.BytesIO(image_bytes)).image
 
     # Display the annotated image
     st.image(prediction_image, caption="Prediction.", use_column_width=True)

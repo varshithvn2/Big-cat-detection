1de1b48de9e8e3e24253e24037972894361ab24a
Varshith <75238274+varshithvn2@users.noreply.github.com>
2024-01-13 08:12:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,22 +14,23 @@ st.title("Big Cat Classification App")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
-    # Get the uploaded image
-    image = Image.open(uploaded_file)
-
     # Save the uploaded image locally
     uploaded_file_path = "uploaded_image.jpg"
+    image = Image.open(uploaded_file)
     image.save(uploaded_file_path)
+
+    # Display a message while the image is being processed
+    st.text("Identifying the Big Cat...")
 
     # Make prediction using Roboflow on the local image
     prediction = model.predict(uploaded_file_path).json()
 
     # Extract the highest probability class and its confidence
     highest_prob_class = prediction['predictions'][0]['predicted_classes'][0]
-    confidence = round(prediction['predictions'][0]['predictions'][highest_prob_class]['confidence'])*100
+    confidence = prediction['predictions'][0]['predictions'][highest_prob_class]['confidence']
 
-    # Display the highest probability class
-    st.write(f"This is a {highest_prob_class}")
+    # Update the message with the prediction result
+    st.text(f"This is a {highest_prob_class} with confidence: {confidence*100:.2f}%")
 
-    # Preview Image
+    # Display the uploaded image
     st.image(image, caption="Uploaded Image.", use_column_width=True)
